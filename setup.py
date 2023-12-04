0188a450f62b326b10e088bb6e1ce5232d9e0b87
Bucciamarcia <info@stefanominiconsulting.com>
2023-12-04 20:33:24 +0200

    Squashed commit of the following:
    commit 6589e34e051433886cf3bf627526ad9d3d46f4ef
    Author: Bucciamarcia <info@stefanominiconsulting.com>
    Date:   Thu Nov 16 07:49:40 2023 +0200
    
        stream funzionante
    
    commit 7edbaf6ede7b904a9f3af23e21ebc59725f475bf
    Author: Bucciamarcia <info@stefanominiconsulting.com>
    Date:   Thu Nov 16 07:24:47 2023 +0200
    
        streaming wip
    
    commit 6a50f7b773da07d2e2a0bce1b6a4765a0ad80f4f
    Author: grit-app[bot] <grit-app[bot]@users.noreply.github.com>
    Date:   Wed Nov 15 18:12:52 2023 +0000
    
        [bot] migrate files

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='libreria_ai_per_tutti',
-    version='0.1.4',
+    version='0.1.5',
     py_modules=['libreria_ai_per_tutti'],
     install_requires=[
-        "openai",
+        "openai>=1.2.0",
         "weaviate-client",
         "tiktoken",
         "langchain",
