e8603a06b32fceed9e7168f99b69c2c357442a62
Stefano Mini <info@stefanominiconsulting.com>
2024-01-03 10:52:31 +0100

    Aggiunge import di colorama (bugfix)

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='libreria_ai_per_tutti',
-    version='0.1.6',
+    version='0.1.7',
     py_modules=['libreria_ai_per_tutti'],
     install_requires=[
         "openai>=1.2.0",
         "weaviate-client",
         "tiktoken",
         "langchain",
+        "colorama",
     ]
 )
